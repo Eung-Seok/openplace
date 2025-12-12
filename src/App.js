55923202801e5207d7f257c8551f0afdae15211e
SONGI <ahnsongi6769@gmail.com>
2025-12-12 16:54:26 +0900

    부트스트랩 및 라우터 설치

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./pages/Hero";
+import Footer from "./components/Footer";
+import FundingSection from "./pages/FundingSection";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Routes, Route } from "react-router";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <FundingSection />
+      <Footer />
+    </>
   );
 }
 
