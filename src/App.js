9ddb448ff7b524c39f8181e9f89f2bed0b0ec051
SONGI <ahnsongi6769@gmail.com>
2025-12-15 11:12:09 +0900

    브라우저 라우터 수정

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from "react";
 import Header from "./components/Header";
-import Hero from "./pages/Hero";
+import Hero from "./components/Hero";
 import Footer from "./components/Footer";
-import FundingSection from "./pages/FundingSection";
+import FundingSection from "./components/FundingSection";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { Routes, Route } from "react-router";
-
+import {Routes, Route } from "react-router";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+import FundingPage from "./pages/FundingPage";
+import CommunityPage from "./pages/CommunityPage";
+import MyPage from "./pages/MyPage";
+import CommunitySection from "./components/CommunitySection";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <FundingSection />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/funding" element={<FundingPage />} />
+                <Route path="/community" element={<CommunityPage />} />
+                <Route path="/mypage" element={<MyPage />} />
+                <Route path="/about" element={<AboutPage />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
