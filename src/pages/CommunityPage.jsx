2c7176ca3dee9c404e35f51751e5fb8c3b55bdde
SONGI <ahnsongi6769@gmail.com>
2025-12-15 11:11:23 +0900

    page 폴더 수정

---
+++ src/pages/CommunityPage.jsx
@@ -0,0 +1,5 @@
+function CommunityPage() {
+    return <div className="container">커뮤니티 페이지</div>;
+}
+
+export default CommunityPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import Hero from "../components/Hero";
+import FundingSection from "../components/FundingSection";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import CommunitySection from "../components/CommunitySection";
+
+function HomePage() {
+    return (
+        <>
+            <header/>
+            <Hero />
+            <FundingSection />
+            <CommunitySection />
+            <Footer/>
+        </>
+    );
+}
+
+export default HomePage;
