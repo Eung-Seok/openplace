a6dc0c51caa49b48c9c4564e80cf38ca827aafee
SONGI <ahnsongi6769@gmail.com>
2025-12-15 12:28:39 +0900

    커뮤니티섹션 top5미리보기 함수 작성

--- src/components/CommunitySection.jsx
+++ src/components/CommunitySection.jsx
@@ -1,43 +1,26 @@
+import Card from "../communitypage/components/Card";
 import "./CommunitySection.css";
 
 function CommunitySection() {
-    const communityTopList = [
-        {
-            id: 1,
-            title: "우리 동네 쓰레기통 부족 문제",
-            likes: 132,
-        },
-        {
-            id: 2,
-            title: "어린이 보호구역 신호등 설치 요청",
-            likes: 89,
-        },
-        {
-            id: 3,
-            title: "노후된 벤치 교체 필요",
-            likes: 64,
-        },
-    ];
+
+    let totalData = JSON.parse(localStorage.getItem('통합데이터'));
+    let likeArr = [];
+    for (let i = 0; i < totalData.length; i++) {
+        likeArr.push(totalData[i].likes)
+    }
+    // 가장 높은 숫자의 인덱스를 구하는 함수 
+    let topData = [...totalData]
+        .slice(0, 5)
 
     return (
         <section className="community-section">
             <div className="container">
                 <h3 className="community-title">
-                    지역 문제 제보 TOP
+                    지역 문제 TOP5
                 </h3>
-
-                <ul className="community-list">
-                    {communityTopList.map((item) => (
-                        <li key={item.id} className="community-item">
-                            <p className="community-item-title">
-                                {item.title}
-                            </p>
-                            <span className="community-like">
-                                <span style={{color:"red"}}> ❤ </span> {item.likes} 공감
-                            </span>
-                        </li>
-                    ))}
-                </ul>
+                {topData.map((item) => (
+                    <Card key={item.id} item={item} />
+                ))}
             </div>
         </section>
     );
