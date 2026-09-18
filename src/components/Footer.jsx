64015e5c0fb5181c8c264f2c3dcf8f319300b413
Eung-Seok <rwjflrk1234@gmail.com>
2026-09-18 08:20:37 -0700

    polish: refine portfolio demo experience

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,48 +10,31 @@ function Footer() {
                 <div className="footer-left">
                     <div className="footer-logo">
                         <img
-                            src="/images/common/OpenPlace.png"
+                            src={`${process.env.PUBLIC_URL}/images/common/OpenPlace.png`}
                             alt="OpenPlace"
                         />
                         <span>OpenPlace</span>
                     </div>
 
                     <ul className="footer-info">
-                        <li><strong>전화번호</strong> 1566-9564</li>
-                        <li><strong>E-mail</strong> openplace@openplace.or.kr</li>
-                        <li><strong>홈페이지</strong> http://www.human.or.kr/</li>
+                        <li>지역 문제 제안부터 펀딩과 후기 공유까지 연결하는 포트폴리오 데모입니다.</li>
                         <li>
-                            <strong>주소</strong>
-                            (31144) 충남 천안시 동남구 대흥로 215 7층, 8층
+                            <strong>Source</strong>
+                            <a href="https://github.com/Eung-Seok/openplace" target="_blank" rel="noreferrer">
+                                GitHub Repository
+                            </a>
                         </li>
                     </ul>
 
-                    <p className="copyright">
-                        copyright© OpenPlace All rights reserved.
-                    </p>
+                    <p className="copyright">© 2026 OpenPlace Portfolio Project</p>
                 </div>
 
                 {/* RIGHT */}
                 <div className="footer-right">
                     <ul className="footer-sns">
                         <li>
-                            <a href="https://www.instagram.com/" aria-label="Instagram">
-                                <i className="bi bi-instagram"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.kakaocorp.com/" aria-label="KakaoTalk">
-                                <i className="bi bi-chat-dots"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.facebook.com/" aria-label="Facebook">
-                                <i className="bi bi-facebook"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.youtube.com/" aria-label="YouTube">
-                                <i className="bi bi-youtube"></i>
+                            <a href="https://github.com/Eung-Seok/openplace" target="_blank" rel="noreferrer" aria-label="GitHub">
+                                <i className="bi bi-github"></i>
                             </a>
                         </li>
                     </ul>
